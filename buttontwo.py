0ee061edcd62d45efb2163a29f7bb0dd318828b5
TomParkwood <39772727+TomParkwood@users.noreply.github.com>
2018-06-26 11:01:45 +0100

    Added a second Doorbell
    Of course two doorbells wouldnt be connected using one Pi, but the code is there so it would just need to be setup on another Pi with the code, I have included instructions on differentiating between the two in the notes.txt

---
+++ buttontwo.py
@@ -0,0 +1,41 @@
+import RPi.GPIO as GPIO
+import time
+import requests
+
+#setup the GPIO pins, they need to be in BCM mode whatever that is
+#6 is the button, this has a pull up resistor
+#21 is the LED which is configured to be an output
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(17,GPIO.OUT)
+
+#at the start of the program make sure the LED is off
+GPIO.output(17,False)
+
+while True:
+	#Get whether or not th button is pressed
+	inputValue = GPIO.input(4)
+	#if the button is pressed then execute these commands
+	if (inputValue == False):
+		#This was for testing it writes button pressed onto the command line
+		#print("Button pressed ")
+		#Set the LED to high
+		GPIO.output(17,True)
+		#This is the curl command stripped down to python so it can be executed
+		data = [
+				('devid', 'vD1216EFF18C0291'),
+				]
+		response = requests.post('http://api.pushingbox.com/pushingbox', data=data)
+		#So there isnt a spam of notifications, this will wait 1 second to see if the button is pressed again
+		time.sleep(1)
+		#after a second turn the led off, if they are still holding the button then the LED will light again
+		GPIO.output(17, False)
+	#So it will wait .3 seconds after the code has been executed to see if the button is still pressed, helps with button ripple
+	time.sleep(0.3)
+	
+#To change the doorbell sound, use the chrome extension, and download the contents of the extension
+#extract it and then change the alert.ogg file, you could change the notifier.js file and change the pb.alertsound to the file you want it to play.
+#On chrome you need to go settings, more tools, then extensions. Enable developer mode.
+#Load unpacked extension up the top and select the folder for the extension.
+#Make sure that in the extension settings under the options then notifications to enable the play a sound when a notification arrives.
+#Test, it should make the doorbell sound when a notification is recieved.
